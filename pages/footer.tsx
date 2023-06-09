262ae1db7e8fd627cc2c63231b7ca917ca29f9f5
RizvanKarimov <karimovrizvan84@gmail.com>
2023-06-09 15:30:26 +0200

    created footer page

--- pages/footer.tsx
+++ pages/footer.tsx
@@ -11,10 +11,16 @@ function Footer() {
       </div>
       <div className="flex gap-8 px-6">
         <div className="flex flex-col gap-3">
-          <h3 className="text-2xl font-bold mb-6">Subscribe Now</h3>
+          <h3 className="text-2xl font-bold mb-1">Subscribe Now</h3>
           <p>There are many variations of passages of Lorem Ipsum available</p>
-          <input type="email" placeholder="Enter Your Email" />
-          <Button className="bg-primary border-0">SUBSCRIBE</Button>
+          <input
+            type="email"
+            placeholder="Enter Your Email"
+            className="bg-transparent border-b-2"
+          />
+          <Button className="bg-primary border-0 rounded py-2 px-2 text-base">
+            SUBSCRIBE
+          </Button>
         </div>
         <div className="flex flex-col gap-3 mb-6">
           <h3 className="text-2xl font-bold">Information</h3>
