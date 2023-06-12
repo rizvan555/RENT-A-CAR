1e414cb3ab85299abf5a9f6174d6457b9d80ff7f
RizvanKarimov <karimovrizvan84@gmail.com>
2023-06-12 10:15:17 +0200

    changed styling in serch page

--- pages/searchContainer.tsx
+++ pages/searchContainer.tsx
@@ -12,21 +12,21 @@ function SearchContainer() {
       </div>
       <div className="md:flex flex-col justify-center items-center md:text-start text-center gap-6 mt-10">
         <form action="" className="md:flex grid grid-cols-1 gap-6">
-          <select className="flex gap-4 w-72 rounded md:text-2xl text-[12px] px-4 py-2">
+          <select className="flex gap-4 w-72 rounded md:text-xl text-[12px] px-4 py-3">
             <option value="AnyBrands">Any Brands</option>
             <option value="Brand1">Brand1</option>
             <option value="Brand2">Brand2</option>
             <option value="Brand3">Brand3</option>
           </select>
 
-          <select className="flex gap-4 w-72 rounded md:text-2xl text-[12px] px-4 py-2">
+          <select className="flex gap-4 w-72 rounded md:text-xl text-[12px] px-4 py-3">
             <option value="AnyType">Any Type</option>
             <option value="Brand1">Type1</option>
             <option value="Brand2">Type2</option>
             <option value="Brand3">Type3</option>
           </select>
 
-          <select className="flex gap-4 w-72 rounded md:text-2xl text-[12px] px-4 py-2">
+          <select className="flex gap-4 w-72 rounded md:text-xl text-[12px] px-4 py-3">
             <option value="Price">Price</option>
             <option value="Brand1">$ 20000</option>
             <option value="Brand2">$ 30000</option>
