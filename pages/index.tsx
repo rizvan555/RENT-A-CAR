a1d680f0bd059cf71acb84833b05fd4765707d46
RizvanKarimov <karimovrizvan84@gmail.com>
2023-06-12 10:53:17 +0200

    correctur for deploying

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import { Inter } from "next/font/google";
 import Catalog from "./catalog";
 import InfoBar from "./infoBar";
-
 import Customers from "./customers";
 import Home from "./home";
 import About from "./about";
 import SearchContainer from "./searchContainer";
 import Contact from "./contact";
-import Footer from "./components/layout/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
