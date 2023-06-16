521ced93c4740b4f8e0a14a8cf8949a77747459c
RizvanKarimov <karimovrizvan84@gmail.com>
2023-06-16 22:36:07 +0200

    deleted example page

--- pages/example.tsx
+++
@@ -1,9 +0,0 @@
-// import { UserButton } from "@clerk/nextjs";
-
-// export default function Example() {
-//   return (
-//     <>
-//       <UserButton afterSignOutUrl="/" />
-//     </>
-//   );
-// }
